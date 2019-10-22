4a72f170cede11f0b8515763842b40180410db06
Luis Pino <lapy0110@gmail.com>
2019-10-22 13:26:36 -0400

    Added BinarySEarchTreeNode for can go in BST

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -34,3 +34,57 @@ class BinaryTreeNode:
 
     def __repr__(self):
         return str(self)
+
+class BinarySearchTreeNode(BinaryTreeNode):
+   
+    def __init__(self, key):
+        BinaryTreeNode.__init__(self, key) 
+
+    def search(self, key):
+        if self == None or self.key == key:
+            return self
+
+        if key < self.key:
+            return self.left.search(key)
+        else:
+            return self.right.search(key)
+
+    def minimum(self):
+        while self != None and self.left != None:
+            self = self.left
+        return self
+
+    def maximum(self):
+        while self != None and self.right != None:
+            self = self.right
+        return self
+
+    def predecessor(self):
+        # Se asume key != None
+        # Caso de subarbol izquierdo no vacio
+        if self.left != None:
+            return self.left.maximum()
+
+        # Caso de subarbol izquierdo vacio
+        # y es el padre de x
+        y = self.p
+        # mientras x sea hijo derecho 
+        while y != None and self == y.left:
+            self = y
+            y = y.p
+        return y
+
+    def successor(self):
+        # Se asume key != None
+        # Caso de subarbol derecho no vacio
+        if self.right != None:
+            return self.right.minimum()
+
+        # Caso de subarbol derecho vacio
+        # y es el padre de x
+        y = self.p
+        # mientras x sea hijo derecho 
+        while y != None and self == y.right:
+            self = y
+            y = y.p
+        return y
